e2c473ee43b084484e05440887771f35349cbc40
Mayank Vishwakarma <censedocean@gmail.com>
2025-03-13 09:19:09 +0530

    add a dynamic function for resolving path of gallery images

--- src/lib/images/stall.tsx
+++ src/lib/images/stall.tsx
@@ -1,24 +1,10 @@
 /** @format */
 
-const stall = [
-	{ id: 36, class: "Stall", imageUrl: "/gallery/Stall/image1.jpg" },
-	{ id: 37, class: "Stall", imageUrl: "/gallery/Stall/image2.jpg" },
-	{ id: 38, class: "Stall", imageUrl: "/gallery/Stall/image3.jpg" },
-	{ id: 39, class: "Stall", imageUrl: "/gallery/Stall/image4.jpg" },
-	{ id: 40, class: "Stall", imageUrl: "/gallery/Stall/image5.jpg" },
-	{ id: 41, class: "Stall", imageUrl: "/gallery/Stall/image6.jpg" },
-	{ id: 42, class: "Stall", imageUrl: "/gallery/Stall/image7.jpg" },
-	{ id: 43, class: "Stall", imageUrl: "/gallery/Stall/image8.jpg" },
-	{ id: 44, class: "Stall", imageUrl: "/gallery/Stall/image9.jpg" },
-	{ id: 45, class: "Stall", imageUrl: "/gallery/Stall/image10.jpg" },
-	{ id: 46, class: "Stall", imageUrl: "/gallery/Stall/image11.jpg" },
-	{ id: 47, class: "Stall", imageUrl: "/gallery/Stall/image12.jpg" },
-	{ id: 48, class: "Stall", imageUrl: "/gallery/Stall/image13.jpg" },
-	{ id: 49, class: "Stall", imageUrl: "/gallery/Stall/image14.jpg" },
-	{ id: 50, class: "Stall", imageUrl: "/gallery/Stall/image15.jpg" },
-	{ id: 51, class: "Stall", imageUrl: "/gallery/Stall/image16.jpg" },
-	{ id: 52, class: "Stall", imageUrl: "/gallery/Stall/image17.jpg" },
-	{ id: 53, class: "Stall", imageUrl: "/gallery/Stall/image18.jpg" },
-	{ id: 54, class: "Stall", imageUrl: "/gallery/Stall/image19.jpg" },
-];
+const totalImages = 46; // Adjust this if new images are added
+const stall = Array.from({ length: totalImages }, (_, index) => ({
+	id: totalImages - index, // Starts from totalImages and goes to 1
+	class: "Stall",
+	imageUrl: `/gallery/Stall/image${totalImages - index}.jpg`,
+}));
+
 export default stall;
